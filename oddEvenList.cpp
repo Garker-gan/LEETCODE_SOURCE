aa9e493b502318d5479f8e37c41e4d463b6f2b58
Garker-gan <348416127@qq.com>
2020-11-13 15:04:55 +0800

    328. 奇偶链表  拆分再合并

---
+++ oddEvenList.cpp
@@ -0,0 +1,35 @@
+/* 
+    328. 奇偶链表  拆分再合并
+    Garker-gan
+    2020-11-13
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head == NULL)    return head;
+        //head指向链表的第一个节点
+        ListNode* evenHead = head->next;
+        ListNode* odd = head;
+        ListNode* even = evenHead;
+        while(even != nullptr && even->next != nullptr)
+        {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = evenHead;
+        return head;
+    }
+};
